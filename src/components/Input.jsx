ff6f6bc3478d1fbe9ab3d338e6d41925f7010609
durjoybd14 <nrd141913@gmail.com>
2022-09-14 00:23:02 +0600

    send button added

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -14,16 +14,68 @@ import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
 import { db, storage } from "../firebase";
 import useScreenSize from "../Hooks/useScreenSize";
-
 const Input = () => {
   const [text, setText] = useState("");
+  console.log(text);
   const [img, setImg] = useState(null);
   const screenSize = useScreenSize();
 
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
 
-  const handleOnEnter = async (text, e) => {
+  const handleOnEnter = async (text) => {
+    if (img) {
+      const storageRef = ref(storage, uuid());
+
+      const uploadTask = uploadBytesResumable(storageRef, img);
+
+      uploadTask.on(
+        (error) => {
+          //TODO:Handle Error
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateDoc(doc(db, "chats", data.chatId), {
+              messages: arrayUnion({
+                id: uuid(),
+                text,
+                senderId: currentUser.uid,
+                date: Timestamp.now(),
+                img: downloadURL,
+              }),
+            });
+          });
+        }
+      );
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    await updateDoc(doc(db, "userChats", currentUser.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    await updateDoc(doc(db, "userChats", data.user.uid), {
+      [data.chatId + ".lastMessage"]: {
+        text,
+      },
+      [data.chatId + ".date"]: serverTimestamp(),
+    });
+
+    setText("");
+    setImg(null);
+  };
+  const handleSend = async () => {
     if (img) {
       const storageRef = ref(storage, uuid());
 
@@ -77,7 +129,7 @@ const Input = () => {
   };
 
   return (
-    <div>
+    <div className="input-send">
       <InputEmoji
         value={text}
         onChange={setText}
@@ -86,6 +138,25 @@ const Input = () => {
         placeholder="Type a message"
         borderRadius={screenSize >= 500 && "0"}
       />
+
+      <div className="send-button" onClick={handleSend}>
+        <svg
+          stroke="currentColor"
+          fill="none"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          height="32px"
+          width="32px"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <desc></desc>
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <line x1="10" y1="14" x2="21" y2="3"></line>
+          <path d="M21 3l-6.5 18a0.55 .55 0 0 1 -1 0l-3.5 -7l-7 -3.5a0.55 .55 0 0 1 0 -1l18 -6.5"></path>
+        </svg>
+      </div>
     </div>
   );
 };
